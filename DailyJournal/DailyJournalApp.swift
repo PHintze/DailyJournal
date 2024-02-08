71c38f5ed99134e933eb7be98c4eb3526cb976de
PHintze <p.hintze159@gmail.com>
2024-02-08 08:31:13 +0100

    Initial Commit

---
+++ DailyJournal/DailyJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyJournalApp.swift
+//  DailyJournal
+//
+//  Created by Pascal Hintze on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DailyJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
